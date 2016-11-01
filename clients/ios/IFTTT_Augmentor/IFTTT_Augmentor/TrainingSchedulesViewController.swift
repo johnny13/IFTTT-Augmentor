dfb7fc700f5025344afa5d464823a37a80f9a7ea
Douglas Wurtele <dougwurtele@gmail.com>
2016-10-31 23:50:12 -0400

    Extracted training schedule manager to separate class

--- clients/ios/IFTTT_Augmentor/IFTTT_Augmentor/TrainingSchedulesViewController.swift
+++ clients/ios/IFTTT_Augmentor/IFTTT_Augmentor/TrainingSchedulesViewController.swift
@@ -10,26 +10,7 @@ import UIKit
 import CoreData
 
 class TrainingSchedulesViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-	private var schedules: Array<TrainingSchedule> = []
 	private let calendarInput = UITextField(frame: CGRect.zero)
-	
-	private var _loadCount: Int = 0
-	private var loadCount: Int {
-		get {
-			return self._loadCount
-		}
-		
-		set {
-			self._loadCount = newValue
-			if _loadCount == 0 {
-				let app = UIApplication.shared.delegate as! AppDelegate
-				app.saveContext()
-				UIApplication.shared.isNetworkActivityIndicatorVisible = false
-			} else {
-				UIApplication.shared.isNetworkActivityIndicatorVisible = true
-			}
-		}
-	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,80 +20,11 @@ class TrainingSchedulesViewController: UITableViewController, UIPickerViewDelega
 		calendarPicker.dataSource = self
 		calendarInput.inputView = calendarPicker
 		self.view.addSubview(calendarInput)
-		
-		let app = UIApplication.shared.delegate as! AppDelegate
-		let context = app.context
-		
-		let fetchRequest: NSFetchRequest<TrainingSchedule> = TrainingSchedule.fetchRequest()
-		self.schedules = try! context.fetch(fetchRequest)
-		self.schedules.sort(by: { lhs, rhs in
-			return lhs.start!.compare(rhs.start as! Date) == .orderedDescending
-		})
-		
-		loadCount = 1
-		URLSession.shared.dataTask(with: URL(string: "http://wurtele.org:8080/IFTTT/api/schedules")!) { data, response, err in
-			if let data = data {
-				if let json = try? JSONSerialization.jsonObject(with: data) {
-					for sched in json as? [AnyObject] ?? [] {
-						let sender = sched["sender"] as! String
-						let file = sched["file"] as! String
-						
-						let id = sender + "/" + file
-						let fetchRequest: NSFetchRequest<TrainingSchedule> = TrainingSchedule.fetchRequest()
-						fetchRequest.predicate = NSPredicate(format: "file = %@", id)
-						do {
-							let results = try context.fetch(fetchRequest)
-							if results.count > 0 {
-								continue
-							}
-						} catch { }
-						let startMillis = sched["start"] as! TimeInterval
-						let endMillis = sched["end"] as! TimeInterval
-						
-						let schedule = TrainingSchedule(context: context)
-						schedule.file = id
-						schedule.start = NSDate(timeIntervalSince1970: startMillis / 1000)
-						schedule.end = NSDate(timeIntervalSince1970: endMillis / 1000)
-						schedule.imported = false
-						
-						self.loadCount += 1
-						URLSession.shared.dataTask(with: URL(string: "http://wurtele.org:8080/IFTTT/api/schedules/\(sender)/\(file)")!) { data, response, err in
-							if let data = data {
-								if let json = try? JSONSerialization.jsonObject(with: data) {
-									for event in json as? [AnyObject] ?? [] {
-										let item = TrainingScheduleItem(context: context)
-										item.schedule = schedule
-										item.start = NSDate(timeIntervalSince1970: event["start"] as! TimeInterval / 1000)
-										item.end = NSDate(timeIntervalSince1970: event["end"] as! TimeInterval / 1000)
-										item.group = event["group"] as? String
-										item.title = event["title"] as? String
-										item.notes = event["notes"] as? String
-										item.instructor = event["instructor"] as? String
-										item.uniform = event["uniform"] as? String
-										item.location = event["location"] as? String
-										schedule.addToItems(item)
-									}
-								}
-							}
-							self.loadCount -= 1
-						}.resume()
-						
-						self.schedules.append(schedule)
-					}
-					self.schedules.sort(by: { lhs, rhs in
-						return lhs.start!.compare(rhs.start as! Date) == .orderedDescending
-					})
-					DispatchQueue.main.async {
-						self.tableView.reloadData()
-					}
-				}
-			}
-			if let e = err {
-				print("Failed to connect to server: \(e)")
-				//TODO: alert user
-			}
-			self.loadCount -= 1
-		}.resume()
+		TrainingScheduleManager.loadTrainingSchedules()
+		//TODO: move this line to a completion handler for the training schedules loader
+		DispatchQueue.main.async {
+			self.tableView.reloadData()
+		}
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -132,7 +44,7 @@ class TrainingSchedulesViewController: UITableViewController, UIPickerViewDelega
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return section == 0 ? 1 : schedules.count
+		return section == 0 ? 1 : TrainingScheduleManager.schedules.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -146,7 +58,7 @@ class TrainingSchedulesViewController: UITableViewController, UIPickerViewDelega
 			return cell
 		} else {
 			let cell = tableView.dequeueReusableCell(withIdentifier: "TrainingScheduleCell", for: indexPath)
-			let schedule = schedules[indexPath.row]
+			let schedule = TrainingScheduleManager.schedules[indexPath.row]
 			cell.textLabel?.text = schedule.dateRange
 			cell.detailTextLabel?.text = schedule.file
 			if let _ = EventManager.armyCalendar {
@@ -209,7 +121,7 @@ class TrainingSchedulesViewController: UITableViewController, UIPickerViewDelega
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if let dest = segue.destination as? TrainingScheduleViewController {
 			if let selectedCell = sender as? UITableViewCell {
-				dest.schedule = self.schedules[self.tableView.indexPath(for: selectedCell)!.row]
+				dest.schedule = TrainingScheduleManager.schedules[self.tableView.indexPath(for: selectedCell)!.row]
 			}
 		}
 		super.prepare(for: segue, sender: sender)
